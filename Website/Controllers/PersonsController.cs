0dc462264c4082046c58ca64ef24c1f6ce71e5a3
JASLAPTOP\Jas <jas3542@hotmail.com>
2019-03-08 11:16:44 +0000

    initial commit

---
+++ Website/Controllers/PersonsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class PersonsController : Controller
+    {
+        private List<Person> _personList;
+
+        public PersonsController()
+        {
+            initializePersonsList(); 
+        }
+        
+        private List<Person> getPersonsList()
+        {
+            return _personList;
+        }
+
+        private void initializePersonsList()
+        {
+            _personList = new List<Person>();
+
+            _personList.Add(new Person("James"));
+            _personList.Add(new Person("Tom"));
+            _personList.Add(new Person("Margarita"));
+            _personList.Add(new Person("Pilar"));
+        }
+
+        public ActionResult Persons()
+        {
+            return View("Person",_personList);
+        }
+    }
+}
---
+++ Website/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class Person
+    {
+        public string _name { get; set; }
+
+        public Person(string namee)
+        {
+            _name = namee;
+        }
+    }
+}
